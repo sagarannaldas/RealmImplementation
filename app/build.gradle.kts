f6d05ed4312d8ee23474f86c33579a5091076778
sagarannaldas <annal7057@gmail.com>
2024-02-17 13:50:56 +0530

    1. added realm lib 2. added 4 schemas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("io.realm.kotlin:library-base:1.11.0")
 }
